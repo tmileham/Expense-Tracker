3ed65e797c0780b4976ab611627f48fa2d3bd3db
Tom Mileham <tom.mileham@hotmail.co.uk>
2022-04-12 22:28:45 +0100

    Feat: Added ExpenseForm Component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
+  const [expenseTitle, setExpenseTitle] = useState(title);
+
+  console.log("ExpenseItem Evaluated");
+  const clickHandler = () => {
+    setExpenseTitle("Updated");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">£{amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
